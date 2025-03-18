4f257c8de2ff2cf8c348a0c8e481071efe07dd2a
robinkov <robinkovacic2@gmail.com>
2025-03-18 14:57:40 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import prokopLogo from './assets/images/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={prokopLogo} width={300}></img>
+      <h2>Stranica je u izradi...</h2>
+    </>
+  )
+}
+
+export default App
